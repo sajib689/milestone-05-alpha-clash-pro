be038f8a177abe809074a3c7cf722ef06830961c
sajib689 <jibonraja11@gmail.com>
2024-02-14 12:17:35 +0600

    genarate alphabet add success

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+function continueGame(){
+   const alphabet = getRandomAlphabet()
+   const currentAlphabet = document.getElementById('current-alphabet')
+   currentAlphabet.innerText = alphabet
+    //    set the background color
+    setBackgroundColor(alphabet)
+}
+
 function play() {
     hideElementById('home-screen')
     showElementById('playground')
+    continueGame()
 }
--- utility.js
+++ utility.js
@@ -6,3 +6,17 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+ 
+function setBackgroundColor (elementById) {
+    const element = document.getElementById(elementById);
+    element.classList.add('bg-orange-400')
+}
+
+function getRandomAlphabet() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+   const randomNumber = Math.random() * alphabets.length
+   const index = Math.round(randomNumber)
+   const alphabet = alphabets[index]
+   return alphabet
+}
